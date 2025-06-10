c69e7240f5887acb9b20c9f279794a1424953e1d
Aman Kumar Gupta <akgbytes@gmail.com>
2025-06-10 07:18:14 +0530

    feat: add two more admin endpoints for managing user role and session

--- backend/src/controllers/admin.controller.ts
+++ backend/src/controllers/admin.controller.ts
@@ -1,28 +1,10 @@
 import { prisma } from "../configs/db";
 import { logger } from "../configs/logger";
+import { UserRole } from "../generated/prisma";
 import { ApiResponse } from "../utils/ApiResponse";
 import asyncHandler from "../utils/asyncHandler";
 import { CustomError } from "../utils/CustomError";
-
-export const deleteUserById = asyncHandler(async (req, res) => {
-  const { userId } = req.params;
-
-  if (!userId) {
-    throw new CustomError(400, "User ID is required");
-  }
-
-  const user = await prisma.user.findUnique({ where: { id: userId } });
-
-  if (!user) {
-    throw new CustomError(404, "User not found");
-  }
-
-  await prisma.user.delete({ where: { id: user.id } });
-
-  logger.info(`Admin deleted user with ID ${userId}`);
-
-  res.status(200).json(new ApiResponse(200, "User deleted successfully", null));
-});
+import { sanitizeUser } from "../utils/sanitizeUser";
 
 export const getAllUsers = asyncHandler(async (req, res) => {
   const users = await prisma.user.findMany({
@@ -75,6 +57,58 @@ export const getUserById = asyncHandler(async (req, res) => {
   res.status(200).json(new ApiResponse(200, "User fetched successfully", user));
 });
 
-export const updateUserById = asyncHandler(async (req, res) => {});
+export const updateUserRole = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+  const { role } = req.body;
 
-export const deleteUserSessionById = asyncHandler(async (req, res) => {});
+  const allowedRoles = Object.values(UserRole);
+  if (!role || !allowedRoles.includes(role)) {
+    throw new CustomError(400, "Invalid or missing role");
+  }
+  const updatedUser = await prisma.user.update({
+    where: { id: userId },
+    data: { role },
+  });
+
+  const safeUser = sanitizeUser(updatedUser);
+
+  logger.info(`Role of ${userId} is updated successfully to ${role}`, {
+    updatedBy: req.user.email,
+  });
+
+  res.status(200).json(new ApiResponse(200, "User role updated successfully", safeUser));
+});
+
+export const deleteUserById = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+
+  if (!userId) {
+    throw new CustomError(400, "User ID is required");
+  }
+
+  const user = await prisma.user.findUnique({ where: { id: userId } });
+
+  if (!user) {
+    throw new CustomError(404, "User not found");
+  }
+
+  await prisma.user.delete({ where: { id: user.id } });
+
+  logger.info(`Admin deleted user with ID ${userId}`);
+
+  res.status(200).json(new ApiResponse(200, "User deleted successfully", null));
+});
+
+export const deleteUserSessionById = asyncHandler(async (req, res) => {
+  const { sessionId } = req.params;
+
+  if (!sessionId) {
+    throw new CustomError(400, "Session ID is required");
+  }
+
+  await prisma.session.delete({ where: { id: sessionId } });
+
+  logger.info(`Session ${sessionId} deleted`, { deletedBy: req.user.email });
+
+  res.status(200).json(new ApiResponse(200, "Session deleted successfully", null));
+});
--- backend/src/routes/admin.route.ts
+++ backend/src/routes/admin.route.ts
@@ -1,14 +1,21 @@
 import { Router } from "express";
-import { updateProfile, changePassword, getProfile } from "../controllers/user.controller";
 
 import { isLoggedIn } from "../middlewares/auth.middleware";
 import { isAdmin } from "../middlewares/role.middleware";
+import {
+  deleteUserById,
+  deleteUserSessionById,
+  getAllUsers,
+  getUserById,
+  updateUserRole,
+} from "../controllers/admin.controller";
 
 const router = Router();
 
-router.get("/users", isLoggedIn, isAdmin);
-router.get("/users/:userId", isLoggedIn, isAdmin);
-router.patch("/users/:userId", isLoggedIn, isAdmin);
-router.delete("/users/:userId", isLoggedIn, isAdmin);
+router.get("/users", isLoggedIn, isAdmin, getAllUsers);
+router.get("/users/:userId", isLoggedIn, isAdmin, getUserById);
+router.patch("/users/:userId", isLoggedIn, isAdmin, updateUserRole);
+router.delete("/users/:userId", isLoggedIn, isAdmin, deleteUserById);
+router.delete("/users/sessions/:sessionId", isLoggedIn, isAdmin, deleteUserSessionById);
 
 export default router;
